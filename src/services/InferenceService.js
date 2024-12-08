158f2a8f8fb2ee2c349443e23151ed165a7140c0
Luukiiy <wicaksono.aldi22@gmail.com>
2024-12-09 04:34:27 +0700

    perbaiki cancer

--- src/services/InferenceService.js
+++ src/services/InferenceService.js
@@ -1,10 +1,10 @@
 const tf = require('@tensorflow/tfjs-node');
 const InputError = require('../exceptions/InputError');
 
-async function predictClassification(model, image) {
+async function predictClassification(model, imageBuffer) {
   try {
     const tensor = tf.node
-      .decodeJpeg(image)
+      .decodeJpeg(imageBuffer)
       .resizeNearestNeighbor([224, 224])
       .expandDims()
       .toFloat()
@@ -13,27 +13,19 @@ async function predictClassification(model, image) {
     const score = await prediction.data();
     const confidenceScore = Math.max(...score) * 100;
 
-    const classes = ['Melanocytic nevus', 'Squamous cell carcinoma', 'Vascular lesion'];
+    const classes = ['Cancer', 'Non-cancer'];
 
     const classResult = tf.argMax(prediction, 1).dataSync()[0];
     const label = classes[classResult];
 
     let explanation, suggestion;
 
-    if (label === 'Melanocytic nevus') {
-      explanation = "Melanocytic nevus adalah kondisi permukaan kulit memiliki bercak warna yang berasal dari sel-sel melanosit, yakni pembentukan warna kulit dan rambut."
-      suggestion = "Segera konsultasi dengan dokter terdekat jika ukuran semakin membesar dengan cepat, mudah luka, atau berdarah."
+    if (label === 'Cancer') {
+      suggestion = "Segera periksa ke dokter!"
     }
   
-    if (label === 'Squamous cell carcinoma') {
-      explanation = "Squamous cell carcinoma adalah jenis kanker kulit yang umum dijumpai. Penyakit ini sering tumbuh pada bagian-bagian tubuh yang sering terkena sinar UV."
-      suggestion = "Segera konsultasi dengan dokter terdekat untuk meminimalisasi penyebaran kanker."
-    }
-  
-    if (label === 'Vascular lesion') {
-      explanation = "Vascular lesion adalah penyakit yang dikategorikan sebagai kanker atau tumor. Penyakit ini sering muncul pada bagian kepala dan leher."
-      suggestion = "Segera konsultasi dengan dokter terdekat untuk mengetahui detail terkait tingkat bahaya penyakit."
-  
+    if (label === 'Non-cancer') {
+      suggestion = "Penyakit kanker tidak terdeteksi."
     }
 
     return { confidenceScore, label, explanation, suggestion };
